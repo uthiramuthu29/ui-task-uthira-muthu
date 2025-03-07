f6c83a1df4c2e22fb45e2f43f896259868dc6be7
uthiramuthu29 <143895746+uthiramuthu29@users.noreply.github.com>
2025-03-07 21:08:20 +0530

    Delete CodeGamaUITask directory

--- CodeGamaUITask/js/um-task.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".tab").forEach((tab) => {
-    tab.addEventListener("click", function () {
-      document.querySelectorAll(".tab").forEach((t) => t.classList.remove("active"));
-      this.classList.add("active");
-    });
-  });
-  
